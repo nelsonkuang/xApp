bd30d524abcfe5d110f8c8f97954a3f217017438
Nelson Kuang <410374308@qq.com>
2017-07-21 08:30:39 +0800

    Update xApp.js

--- xApp.js
+++ xApp.js
@@ -12,7 +12,7 @@
             }
         },
         getAppVersion: function () { // 比如：APP输出到webview的userAgent里面的串带定义好的 yourAppName/1.1-debug 则调用方法xApp.getAppVersion('yourAppName') => '1.1-debug'
-            var reg = new RegExp('(^|\/)' + appName + '\/([\w-_\.]+)(\/|$)', 'i');
+            var reg = new RegExp('(^|\\s)' + appName + '\/([\\w-_\.]+)(\/|$)', 'i');
             var r = navigator.userAgent.match(reg);
             if (r != null) {
                 return r[2];
